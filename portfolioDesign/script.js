6bcc24e20280c7e2c619f3b04a2fb3a2b377fc6d
bilalquresi <40652101+bilalquresi@users.noreply.github.com>
2021-07-07 00:15:26 +0500

    Add files via upload

---
+++ portfolioDesign/script.js
@@ -0,0 +1,48 @@
+const slidercontainer = document.querySelector('.Slider-container')
+const slideright = document.querySelector('.Right-slider')
+const sliderleft = document.querySelector('.Left-slider')
+const upbutton = document.querySelector('.up-button')
+const downbutton = document.querySelector('.down-button')
+const slidelength = slideright.querySelectorAll('div').length
+const leftdiv = sliderleft.querySelectorAll('div')
+var colorarray = []
+const array = leftdiv.forEach((element) => colorarray.push(element.style.backgroundColor));
+colorarray = colorarray.reverse()
+
+let slideindex = 0;
+let previousindex = 0;
+sliderleft.style.top = `-${(slidelength-1) * 100}vh`
+
+
+downbutton.addEventListener("mouseleave" , ()=>downbutton.style.color = '#A7BBC7')
+upbutton.addEventListener("mouseleave" , ()=>upbutton.style.color = '#A7BBC7')
+upbutton.addEventListener('click', ()=>changeslide('up'))
+downbutton.addEventListener('click', () =>changeslide('down'))
+downbutton.addEventListener("mouseover" , ()=>colorchange(downbutton))
+upbutton.addEventListener("mouseover" , ()=>colorchange(upbutton))
+
+function colorchange(direction){
+	
+		direction.style.color = colorarray[slideindex]
+}
+function changeslide(direction){
+
+	const sliderheight = slidercontainer.clientHeight
+
+	if(direction === "up"){
+		slideindex++
+		if(slideindex > slidelength - 1){
+			slideindex = 0
+			
+		}
+	}
+	else if(direction === "down"){
+		    slideindex--
+			if(slideindex < 0){
+				
+				slideindex = slidelength - 1
+			}
+	}
+	slideright.style.transform = `translateY(-${slideindex * sliderheight}px)`
+	sliderleft.style.transform = `translateY(${slideindex * sliderheight}px)`
+	}
